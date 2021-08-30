436fb1f4ec0f8655dba0513c16b4e8c5c24afb07
Natalie Barba <nbarba2010@gmail.com>
2021-08-30 09:43:25 -0700

    commit tree 74af71e689889b94b9fda66266de51e2041d7f0e

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>During my free time, I love cuddling with my 2 dogs, Levi and Snoop.</p>
+      <img src={image} alt="I made this"></img>
+    </div>
+  );
 }
 
 export default About;
